c7e79509008ebb3116055f995f2fd29acd3a7b06
taka14110165 <taka14110165@gmail.com>
2018-08-06 05:46:42 -0400

    message

--- myjs.js
+++ myjs.js
@@ -4,11 +4,7 @@
 alert((Math.sqrt(2))+(Math.sqrt(3)));
 //alert(Math.sqrt(1/0));
 //alert(Math.sqrt(0/0));
-alert(2+3);
-alert(`2`+`3`);
-alert(Number(`2`)+Number(`3`));
-alert(`2`+3);
-alert(Number(`2`)+3);
+
 
 
 
